c810ffe6875909041e6b0d3188e1d3f4a7143f0d
Progress Aienobe <paienobe@gmail.com>
2024-03-24 16:50:16 +0100

    0-suggestions-state

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,13 +2,19 @@ import Bulb from "../svgs/Bulb";
 import styles from "./Navbar.module.css";
 import DownArrow from "../svgs/DownArrow";
 import Button from "../ui/Button/Button";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store";
 
 const Navbar = () => {
+  const suggestions = useSelector(
+    (state: RootState) => state.productRequests.value
+  );
+
   return (
     <nav className={styles.navbar}>
       <div>
         <Bulb />
-        <h1>6 Suggestions</h1>
+        <h1>{suggestions.length} Suggestions</h1>
       </div>
 
       <button>
--- src/components/RequestHolder/RequestHolder.tsx
+++ src/components/RequestHolder/RequestHolder.tsx
@@ -2,6 +2,8 @@ import { RootState } from "../../store/store";
 import ProductRequest from "../ProductRequest/ProductRequest";
 import styles from "./RequestHolder.module.css";
 import { useSelector } from "react-redux";
+import emptyIllustration from "../../assets/suggestions/illustration-empty.svg";
+import Button from "../ui/Button/Button";
 
 const RequestHolder = () => {
   const productRequests = useSelector(
@@ -9,9 +11,29 @@ const RequestHolder = () => {
   );
   return (
     <section className={styles.request_holder}>
-      {productRequests.map((request) => {
-        return <ProductRequest key={request.id} request={request} />;
-      })}
+      {productRequests.length > 0 ? (
+        <>
+          {productRequests.map((request) => {
+            return <ProductRequest key={request.id} request={request} />;
+          })}
+        </>
+      ) : (
+        <section className={styles.empty_holder}>
+          <img src={emptyIllustration} alt="" />
+          <h1>There is no feedback yet.</h1>
+          <p>
+            Got a suggestion? Found a bug that needs to be squashed? <br /> We
+            love hearing about new ideas to improve our app.
+          </p>
+          <Button
+            text="+ Add Feedback"
+            width="9.75rem"
+            height="2.75rem"
+            backgroundColor="var(--purple)"
+            onClick={() => {}}
+          />
+        </section>
+      )}
     </section>
   );
 };
