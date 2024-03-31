8d60286b03c031b825bcc39629d50c78d96188cd
Progress Aienobe <paienobe@gmail.com>
2024-03-31 13:20:22 +0100

    edit-page-setup

---
+++ src/pages/Edit/Edit.tsx
@@ -0,0 +1,12 @@
+import BackButton from "../../components/layouts/BackButton/BackButton";
+import styles from "./Edit.module.css";
+
+const Edit = () => {
+  return (
+    <section className={styles.edit_page}>
+      <BackButton />
+    </section>
+  );
+};
+
+export default Edit;
